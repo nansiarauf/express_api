898d5a9f9935560411859237a1c9fe5c438c41b7
Abdul Rauf Nansia <nansia.rauf@gmail.com>
2021-04-06 14:21:32 +0000

    express handlebars implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const exhb = require("express-handlebars");
 const dataRoutes = require("./routes/dataRoutes");
 
 //SETTING SERVER PORT
@@ -7,6 +8,15 @@ const lPort = process.env.PORT || 1000;
 
 //MIDDLEWARE AND BODY PARSER
 app.use(express.json());
+
+//INIT HANDLEBARS MIDDLEWARE
+app.engine("handlebars", exhb({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//DEFAULT MAIN
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 //INITIALIZING THE ROUTES
 app.use("/api/v1/data", dataRoutes);
@@ -16,4 +26,4 @@ app.listen(lPort, () => {
   console.log(`Server started on PORT:${lPort}`);
 });
 
-module.exports = app;
+// module.exports = app;
